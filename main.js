2721b982b447e15e530c5e4f04bb7c6d2afecbb6
Andrew Hepworth <hepworta@oregonstate.edu>
2019-11-20 15:46:52 -0800

    Added counter
    Added a counter. It kinda works I guess.

--- main.js
+++ main.js
@@ -10,3 +10,11 @@ function updateRideCount(data) {
   numberOfRides = data.count
   $("h4#rideCount").html(numberOfRides)
 }
+$(document).ready(function() {
+const countUp = new CountUp('rideCount',0, 19777);
+if (!countUp.error) {
+  countUp.start();
+} else {
+  console.error(countUp.error);
+}
+})
