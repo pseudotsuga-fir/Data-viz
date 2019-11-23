180dc6db120ce373d0fd852265f8d8bf0fe823d0
Andrew Hepworth <hepworta@oregonstate.edu>
2019-11-23 15:20:12 -0800

    Project Milestone 2
    Added example from chart.js to page to ready for own data viz.

--- main.js
+++ main.js
@@ -7,14 +7,35 @@ function updateView() {
 }
 
 function updateRideCount(data) {
-  numberOfRides = data.count
-  $("h4#rideCount").html(numberOfRides)
+  numberOfRides == data.count
+  /*$("h4#rideCount").html(numberOfRides)*/
 }
 $(document).ready(function() {
-const countUp = new CountUp('rideCount',0, 19777);
+const countUp = new CountUp('rideCount',0, 192777);
 if (!countUp.error) {
   countUp.start();
 } else {
   console.error(countUp.error);
 }
+})
+$(document).ready(function(){
+var ctx = document.getElementById('canvas').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
 })
