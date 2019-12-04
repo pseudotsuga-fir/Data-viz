78a475d21ac7ee3c2d32c47bbf8be2107df89cc9
Andrew Hepworth <hepworta@oregonstate.edu>
2019-12-04 00:37:37 -0800

    Finalized Dataviz
    Finalized the project: stylized the page, added about section, added chartjs options, set up data.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Data-viz
+This is a visualization that shows the total number of bike departures per month from the top three most trafficked <br> Zagster bike rental stations in Bend, OR 
+during 2018. Zagster is a ride share program that allows users to rent bikes by the hour. <br> The top three most trafficked bike stations were at OSU cascades 
+(Oregon State University campus in Bend), the Old Mill <br> (a shopping district along side the Deschutes river), and Drake park (A frequented park along the Deschutes
+river). The monthly<br>  totals allow us to see the affect the college students have on the bikes, with highs at the college campus in September; the start of <br>  OSU’s fall
+term. This visualization is made using chart.js and jQuery, which are JavaScript libraries that allow for easy data <br>  visualization and working with data. The data
+comes from an external database and is fetched used jQuery. The data is then <br> narrowed down to what is needed, in this case the number of ride uses by location per
+month, and then fed into chart.js to visualize. 
--- main.js
+++ main.js
@@ -2,40 +2,118 @@ const BASE_URL = "https://zagster-service.herokuapp.com"
 
 $(updateView)
 
-function updateView() {
-  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
-}
+var osu_data = []
+var om_data = []
+var dp_data = []
 
-function updateRideCount(data) {
-  numberOfRides == data.count
-  /*$("h4#rideCount").html(numberOfRides)*/
+function updateView() {
+  $.when ($.getJSON(BASE_URL + "/rides/count/osu_cascades/per_month", m_osu),
+  $.getJSON(BASE_URL + "/rides/count/old_mill/per_month", m_om),
+  $.getJSON(BASE_URL + "/rides/count/drake_park/per_month", m_dp)
+    ).then(updateChart);
 }
-$(document).ready(function() {
-const countUp = new CountUp('rideCount',0, 192777);
-if (!countUp.error) {
-  countUp.start();
-} else {
-  console.error(countUp.error);
+function m_osu(data) {
+  var OSUdata2018 = data[2018]
+  iterate(osu_data,OSUdata2018)
+  console.log(osu_data)
+  //console.log(OSUdata2018[11][12])
+  //Oh my god I can't believe that worked
 }
-})
-$(document).ready(function(){
+function m_om(data) {
+  var OMdata2018 = data[2018]
+  iterate(om_data,OMdata2018)
+  console.log(om_data)
+}
+function m_dp(data) {
+  var DPdata2018 = data[2018]
+  dp_data.push(0)
+  iteratedp(dp_data,DPdata2018)
+  console.log(dp_data)
+}
+//This function adds each query result(LocRaw) into an array(LocArray)
+function iterate(LocArray,LocRaw){
+let i=0;
+do {
+  LocArray.push(LocRaw[i][i+1])
+  i += 1;
+} while (i < 10);
+}
+//This function does literally the same thing as the one above, but it adds a two to the second part of the query because the Drake Park data refused it any other way.
+function iteratedp(LocArray,LocRaw){
+  let i=0;
+  do {
+    LocArray.push(LocRaw[i][i+2])
+    i += 1;
+  } while (i < 9);
+  }
+
+function updateChart() {
+  $(document).ready(function() {
+    const countUp = new CountUp('rideCount',0, 192777);
+    if (!countUp.error) {
+      countUp.start();
+    } else {
+      console.error(countUp.error);
+    }
+    })
+  $("#mid").fadeIn(1500,"swing");
+  $("#rideCount").fadeIn(1500,"swing");
 var ctx = document.getElementById('canvas').getContext('2d');
 var chart = new Chart(ctx, {
-    // The type of chart we want to create
     type: 'line',
-
-    // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October'],
+      datasets: [{
+          label: 'OSU Cascades',
+          backgroundColor: 'rgb(204, 228, 255,0.2)' ,
+          borderColor: 'rgb(204, 228, 255)',
+          data: osu_data
+      },
+      {
+        label: 'Old Mill',
+        backgroundColor: 'rgb(204, 255, 84,0.2)',
+        pointBackgroundColor:'rgb(204, 255, 84)',
+        borderColor: 'rgb(204, 255, 84)',
+        data: om_data
+      },
+      {
+        label: 'Drake Park ',
+        backgroundColor: 'rgb(201, 180, 247,0.2)',
+        borderColor: 'rgb(201, 180, 247)',
+        data: dp_data
+        
+      },
+      
+    ]
     },
 
     // Configuration options go here
-    options: {}
+    options: {
+      animation: {
+        duration: 2500,
+        easing: 'easeInOutExpo',
+      },
+      tooltips: {
+        mode: 'index',
+      },
+      hover: {
+        mode: 'index'
+      },
+      scales: {
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Month'
+          }
+        }],
+        yAxes: [{
+          stacked: false,
+          scaleLabel: {
+            display: true,
+            labelString: 'Total Rides'
+          }
+        }]
+      }
+    }
 });
-})
+}
